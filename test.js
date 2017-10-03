38073da7ef9eddf13b8476731b4e2de47cfa915d
sauravyash <yash_agasti@hotmail.com>
2017-10-03 21:10:41 +1100

    Attempt at fixing CircleCI
    Edit to circle yaml

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
-var run = require('tape-run');
-var browserify = require('browserify');
+// A simple test to verify a visible window is opened with a title
+var Application = require('spectron').Application
+var assert = require('assert')
 
-browserify('./test/test.js')
-  .bundle()
-  .pipe(run())
-  .on('results', console.log)
-  .pipe(process.stdout);
+var app = new Application({
+  path: '/Applications/MyApp.app/Contents/MacOS/MyApp'
+})
+
+app.start().then(function () {
+  // Check if the window is visible
+  return app.browserWindow.isVisible()
+}).then(function (isVisible) {
+  // Verify the window is visible
+  assert.equal(isVisible, true)
+}).then(function () {
+  // Get the window's title
+  return app.client.getTitle()
+}).then(function (title) {
+  // Verify the window's title
+  assert.equal(title, 'My App')
+}).then(function () {
+  // Stop the application
+  return app.stop()
+}).catch(function (error) {
+  // Log any failures
+  console.error('Test failed', error.message)
+})
