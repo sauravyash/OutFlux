6348b9031a22a364b539b3c25b5462943cb22b54
sauravyash <yash_agasti@hotmail.com>
2017-10-04 22:51:02 +1100

    Fixed casing
    And added debug logging to test script

--- test.js
+++ test.js
@@ -3,22 +3,26 @@ var Application = require('spectron').Application
 var assert = require('assert')
 
 var app = new Application({
-  path: 'OutFlux-linux-x64/OutFlux'
+  path: 'outFlux-linux-x64/outflux'
 })
 
 app.start().then(function () {
   // Check if the window is visible
-  return app.browserWindow.isVisible()
+  return app.browserWindow.isVisible();
 }).then(function (isVisible) {
+  console.log('Browser Window is visible');
   // Verify the window is visible
-  assert.equal(isVisible, true)
+  assert.equal(isVisible, true);
 }).then(function () {
+  console.log('Browser Window is visible (verified)');
   // Get the window's title
   return app.client.getTitle()
 }).then(function (title) {
+  console.log('Browser Window title matched to outflux');
   // Verify the window's title
-  assert.equal(title, 'OutFlux')
+  assert.equal(title, 'outflux')
 }).then(function () {
+  console.log('verified above');
   // Stop the application
   return app.stop()
 }).catch(function (error) {
